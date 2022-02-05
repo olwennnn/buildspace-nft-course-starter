c5f335d5168fc9692b58fc4dd93a53c38b46ef60
olwn <59951862+olwennnn@users.noreply.github.com>
2022-02-05 12:31:15 +0700

    #2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
+import myEpicNft from './utils/MyEpicNFT.json'
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -50,6 +51,32 @@ const App = () => {
     }
   }
 
+  const askContractToMintNFT = async () => {
+    const CONTRACT_ADDRESS = "0xF5014a8daD4fa08Ab4AE50285a40940915CB6fFE";
+
+    try{
+      const {ethereum} = window;
+
+      if(ethereum){
+        const provider = new ether.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining... please wait.");
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+      }else{
+        console.log("Ethereum object doesn't exist!");
+      }
+    }catch (error){
+      console.log(error);
+    }
+  }
+
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
@@ -71,7 +98,7 @@ const App = () => {
           </p>
 
           {currentAccount === "" ? renderNotConnectedContainer() : (
-            <button onClick={null} className="cta-button connect-wallet-button">
+            <button onClick={askContractToMintNFT} className="cta-button connect-wallet-button">
               Mint NFT
             </button>
           )}
@@ -80,7 +107,7 @@ const App = () => {
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          
+
           <a
             className="footer-text"
             href={TWITTER_LINK}
