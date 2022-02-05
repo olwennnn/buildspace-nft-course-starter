f0688910518de3322fea096fbff28a9e761091df
olwn <59951862+olwennnn@users.noreply.github.com>
2022-02-05 13:53:06 +0700

    #3

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const App = () => {
         const signer = provider.getSigner();
         const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
 
-        connectedContract,on("NewEpicNFTMinted", (from, tokenId) => {
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber());
           alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
         })
